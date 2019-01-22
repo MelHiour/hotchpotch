f63a30673c13b5e0eacfac9801068923cf7bca1d
Mikhail Ermakov <melhiour@mac.pc.hi>
2019-01-22 07:11:41 +0300

    Added a clock script fro RaspberryPi

---
+++ py_clock/clock.py
@@ -0,0 +1,24 @@
+import time
+import Adafruit_DHT
+import sys
+from datetime import datetime
+
+from luma.led_matrix.device import max7219
+from luma.core.interface.serial import spi, noop
+from luma.core.virtual import viewport, sevensegment
+
+serial = spi(port=0, device=0, gpio=noop())
+device = max7219(serial, cascaded=1)
+seg = sevensegment(device)
+
+while True:
+    for i in range(10):
+        now = datetime.now()
+        if i % 2 == 0:
+            seg.text = '- ' + now.strftime("%H.%M") + ' -'
+        else:
+            seg.text = '- ' + now.strftime("%H%M") + ' -'
+        time.sleep(1)
+    humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 17)
+    seg.text = '{0:0.1f}C {1:0.1f}H'.format(temperature, humidity)
+    time.sleep(10)
