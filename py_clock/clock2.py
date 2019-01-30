4ec85089aaf5e25ada4a53cdeb554a18aa4347c9
Mikhail Ermakov <melhiour@gmail.com>
2019-01-30 08:33:37 +0300

    minor changes

---
+++ py_clock/clock2.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+import time
+import re
+import requests
+import Adafruit_DHT
+from datetime import datetime
+
+from luma.led_matrix.device import max7219
+from luma.core.interface.serial import spi, noop
+from luma.core.virtual import viewport, sevensegment
+
+serial = spi(port=0, device=0, gpio=noop())
+device = max7219(serial, cascaded=1)
+seg = sevensegment(device)
+
+seg.text = 'LOVECECA'
+for x in range(5):
+    for intensity in range(16):
+        seg.device.contrast(intensity * 16)
+        time.sleep(0.1)
+
+device.contrast(0x7F)
+seg.device.contrast(16)
+
+while True:
+    humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 17)
+    r = requests.get('http://wttr.in/?format=3')
+    outside_temp = re.search(' (-?\d{1,2})', r.text)
+
+    now = datetime.now()
+    seg.text = '{first:>4.1f}C{second}'.format(second=now.strftime("%H.%M"), first=temperature)
+    time.sleep(2)
+
+    now = datetime.now()
+    seg.text = '{first:>4.1f}H{second}'.format(second=now.strftime("%H.%M"), first=humidity)
+    time.sleep(2)
+
+    now = datetime.now()
+    seg.text = '{first:>3}T{second}'.format(second=now.strftime("%H.%M"), first=outside_temp.group(1))
+    time.sleep(2)
+
+'''
+    if datetime.timetuple(now)[4] == 0:
+        with open('/root/temp-data', 'a') as file:
+            file.write('{} {} {} {}\n'.format(str(now), str(temperature), str(humidity), outside_temp.group(1)))
+        for i in range(60):
+            seg.text = '{:>03} {}'.format(str(i), now.strftime("%H.%M"))
+            time.sleep(1)
+'''
