c6fe67a4d708bee3d0421b3f649811a67af14d19
root <root@raspberrypi>
2019-02-02 16:36:03 +0300

    get_db_data tested

--- py_clock/temp-web.py
+++ py_clock/temp-web.py
@@ -1,9 +1,11 @@
 import sqlite3
 from flask import Flask
 
-def get_db_data(db_path, table_name):
+def get_db_data(db_path, table_name, from_date, until_date):
     with sqlite3.connect(db_path) as connection:
-        result = connection.fetchall('select * from {}'.format(table_name))
+        cursor = connection.cursor()
+        cursor.execute("select * from {} where date between '{}' and '{}'".format(table_name, from_date, until_date))
+        result = cursor.fetchall()
     return result
 '''
 app = Flask(__name__)
@@ -13,4 +15,5 @@ def hello():
 '''
 if __name__ == '__main__':
     #app.run(host='0.0.0.0', debug=True)
-    result = get_db_data('/root/temp-data/temp-data.db', 'weather')
+    result = get_db_data('/root/temp-data/temp-data.db', 'weather', '2019-02-01', '2019-02-02')
+    print(result)
