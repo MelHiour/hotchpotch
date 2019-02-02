2fd0ae50897972b1ee387b68f14faa928a0a7960
Mikhail Ermakov <melhiour@gmail.com>
2019-02-02 15:35:12 +0300

    get_db_data

--- py_clock/temp-web.py
+++ py_clock/temp-web.py
@@ -1,10 +1,16 @@
+import sqlite3
 from flask import Flask
 
+def get_db_data(db_path, table_name):
+    with sqlite3.connect(db_path) as connection:
+        result = connection.fetchall('select * from {}'.format(table_name))
+    return result
+'''
 app = Flask(__name__)
-
 @app.route('/')
 def hello():
     return 'Hello!'
-
+'''
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    #app.run(host='0.0.0.0', debug=True)
+    result = get_db_data('/root/temp-data/temp-data.db', 'weather')
