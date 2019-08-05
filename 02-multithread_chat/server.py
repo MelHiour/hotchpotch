82972949aa81d377d9b87901c228435d8491d17c
Mikhail Ermakov <melhiour@gmail.com>
2019-08-05 10:44:40 +0300

    Adding some comments

--- 02-multithread_chat/server.py
+++ 02-multithread_chat/server.py
@@ -4,8 +4,8 @@ import modules
 HOST = modules.HOST
 PORT = modules.PORT
 
-send_queues = {}                                                  
-lock = threading.Lock()
+send_queues = {}                                                # Define a dict(send_queues)  
+lock = threading.Lock()                                         # Define a LOCK. Will be used for queues precessing
 
 def handle_client_recv(sock, addr):
    """ Receive messages from client and broadcast them to
@@ -13,31 +13,31 @@ def handle_client_recv(sock, addr):
    rest = bytes()                                               # Define rest because it's used later
    while True:
        try:
-           (msgs, rest) = modules.recv_msgs(sock, rest)         # list(msgs) and rest iare returned
+           (msgs, rest) = modules.recv_msgs(sock, rest)         # list(msgs) and rest are returned
        except (EOFError, ConnectionError):
            handle_disconnect(sock, addr)
            break
        for msg in msgs:
            msg = '{}: {}'.format(addr, msg)
            print(msg)
-           broadcast_msg(msg)
+           broadcast_msg(msg)                                   # See func below
 
 def handle_client_send(sock, q, addr):
    """ Monitor queue for new messages, send them to client as
        they arrive """
    while True:
-       msg = q.get()
+       msg = q.get()                                            # Get a message from particular queue
        if msg == None: break
        try:
-           modules.send_msg(sock, msg)
+           modules.send_msg(sock, msg)                          # Transmit a message
        except (ConnectionError, BrokenPipe):
            handle_disconnect(sock, addr)
            break
 
 def broadcast_msg(msg):
    """ Add message to each connected client's send queue """
-   with lock:
-       for q in send_queues.values():
+   with lock:                                                  # LOCK 
+       for q in send_queues.values():                          # for every queue add a message for transfer
            q.put(msg)
 
 def handle_disconnect(sock, addr):
@@ -46,12 +46,12 @@ def handle_disconnect(sock, addr):
    fd = sock.fileno()
    with lock:
        # Get send queue for this client
-       q = send_queues.get(fd, None)
+       q = send_queues.get(fd, None)                           # Get a queue from message_queues with LOCK
    # If we find a queue then this disconnect has not yet
    # been handled
    if q:
-       q.put(None)
-       del send_queues[fd]
+       q.put(None)                                             # ?
+       del send_queues[fd]                                     # Delete a queue from dictionary
        addr = sock.getpeername()
        print('Client {} disconnected'.format(addr))
        sock.close()
