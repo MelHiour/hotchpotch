faaa9a1d3db4d92d547ece65f16a1e853267a50e
Mikhail Ermakov <melhiour@mac.pc.hi>
2019-08-06 11:10:04 +0300

    Commets added

--- 05-asyncio_chat/server.py
+++ 05-asyncio_chat/server.py
@@ -10,16 +10,16 @@ class ChatServerProtocol(asyncio.Protocol):
     """ Each instance of class represents a client and the socket
       connection to it. """
 
-    def connection_made(self, transport):
+    def connection_made(self, transport):                                 # is called when new client connetc (same as socket.accept())
         """ Called on instantiation, when new client connects """
-        self.transport = transport
+        self.transport = transport                                        # asyncio transport object used fo writing data to the socket
         self.addr = transport.get_extra_info('peername')
         self._rest = b''
         clients.append(self)
         print('Connection from {}'.format(self.addr))
 
-    def data_received(self, data):
-        """ Handle data as it's received. Broadcast complete
+    def data_received(self, data):                                        # is called when asyncio.Protocol receives data
+        """ Handle data as it's received. Broadcast complete              # an equivalent of poll.poll() with POLLIN
         messages to all other clients """
         data = self._rest + data
         (msgs, rest) = modules.parse_recvd_data(data)
@@ -30,7 +30,7 @@ class ChatServerProtocol(asyncio.Protocol):
             print(msg)
             msg = modules.prep_msg(msg)
         for client in clients:
-           client.transport.write(msg)  # <-- non-blocking
+           client.transport.write(msg)  # <-- non-blocking              # where the data is sending. Submit data to event loop
 
     def connection_lost(self, ex):
        """ Called on client disconnect. Clean up client state """
@@ -38,15 +38,15 @@ class ChatServerProtocol(asyncio.Protocol):
        clients.remove(self)
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
+    loop = asyncio.get_event_loop()                         # create a loop
     # Create server and initialize on the event loop
-    coroutine = loop.create_server(ChatServerProtocol,
+    coroutine = loop.create_server(ChatServerProtocol,      # Instantiate a object from class asyncio.Protocol
                                 host=HOST,
                                 port=PORT)
-    server = loop.run_until_complete(coroutine)
+    server = loop.run_until_complete(coroutine)             # Run loop. Now clients are allowed to connect
     # print listening socket info
-    for socket in server.sockets:
+    for socket in server.sockets:                           # Just for printing. All real processing is under Class
         addr = socket.getsockname()
         print('Listening on {}'.format(addr))
     # Run the loop to process client connections
-    loop.run_forever()
+    loop.run_forever()                                      # Run forever
