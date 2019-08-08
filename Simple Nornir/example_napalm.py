4019ee71eacda42c974b173e88aecabbd6d9176a
Mikhail Ermakov <melhiour@mac.pc.hi>
2019-08-08 10:37:35 +0300

    Playing with output. Created a nested dictionary

--- Simple Nornir/example_napalm.py
+++ Simple Nornir/example_napalm.py
@@ -1,8 +1,18 @@
 from nornir import InitNornir
-from nornir.plugins.tasks import networking
+from nornir.plugins.tasks import networking, text
 from nornir.plugins.functions.text import print_result
+from pprint import pprint
 
 nr = InitNornir(config_file="config.yaml", logging={"file": "mylogs", "level": "debug"})
 routers = nr.filter()
 result = routers.run(task=networking.napalm_get, getters=["facts"])
-print_result(result)
+
+# Just printing it
+# print_result(result)
+
+# Iterate and create a dictionary from print_result
+result_dict = {}
+for item, value in result.items():
+    result_dict[item] = value.result['facts']
+
+pprint(result_dict)
