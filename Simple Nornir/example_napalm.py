9b700de081993a7f66562aea2653535abb861556
Mikhail Ermakov <melhiour@mac.pc.hi>
2019-08-08 10:38:24 +0300

    Disable debugging

--- Simple Nornir/example_napalm.py
+++ Simple Nornir/example_napalm.py
@@ -3,7 +3,9 @@ from nornir.plugins.tasks import networking, text
 from nornir.plugins.functions.text import print_result
 from pprint import pprint
 
-nr = InitNornir(config_file="config.yaml", logging={"file": "mylogs", "level": "debug"})
+nr = InitNornir(config_file="config.yaml",
+#                logging={"file": "mylogs", "level": "debug"}       # <- uncomment for debug
+                )
 routers = nr.filter()
 result = routers.run(task=networking.napalm_get, getters=["facts"])
 
