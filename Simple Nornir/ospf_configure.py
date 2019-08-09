ade1eef47b7ccb648d02b045e92bdac719bc8304
Mikhail Ermakov <melhiour@mac.pc.hi>
2019-08-09 21:41:04 +0300

    task.run(task=netmiko_save_config) added

--- Simple Nornir/ospf_configure.py
+++ Simple Nornir/ospf_configure.py
@@ -1,7 +1,7 @@
 import ipaddress as ipad
 from nornir import InitNornir
 from nornir.plugins.tasks.text import template_file
-from nornir.plugins.tasks.networking import netmiko_send_config, napalm_get
+from nornir.plugins.tasks.networking import netmiko_send_config, netmiko_save_config, napalm_get
 from nornir.plugins.functions.text import print_result
 
 nr = InitNornir(config_file="config.yaml")
@@ -12,6 +12,7 @@ def ospf_enabled(host):
 def configure_ospf(task):
     task.host['template_config'] = task.run(task=template_file, template='ospf_template.j2', path='templates')
     task.run(task=netmiko_send_config, config_commands=task.host['template_config'].result)
+    task.run(task=netmiko_save_config)
     task.run(task=napalm_get, getters=['facts', 'get_interfaces_ip'])
 
 routers = nr.filter(filter_func=ospf_enabled)
