94f8b118222c94c8514499f0715652802889f2fd
Mikhail Ermakov <melhiour@mac.pc.hi>
2019-08-13 10:10:00 +0300

    Simple NETCONF/YANG example

--- Simple NETCONF-YANG/example.py
+++ Simple NETCONF-YANG/example.py
@@ -1,4 +1,5 @@
 import xmltodict
+from jinja2 import Environment, FileSystemLoader
 from ncclient import manager
 from pprint import pprint
 
@@ -9,6 +10,21 @@ CONNECTION = {
     'password': 'cisco',
     'hostkey_verify': False
 
+}
+
+CONFIG_DATA = {
+'loopbacks': [
+    {'id': '0',
+    'description': 'Loopback number one',
+    'address': '10.1.1.1',
+    'netmask': '255.255.255.0'
+    },
+    {'id': '1',
+    'description': 'Loopback number two',
+    'address': '10.1.1.2',
+    'netmask': '255.255.255.0'
+    }
+]
 }
 
 def print_intfs_xmltodict(connection):
@@ -25,5 +41,24 @@ def print_intfs_xmltodict(connection):
                 intf_mask = interface['ip']['address']['primary']['mask']
                 print(f'You have interface with number {intf_name} and primary IP {intf_ip}/{intf_mask}')
 
+def cfg_from_template(template_file, params, template_dir='templates'):
+    """ Generate a configuration from template using parameters.
+    ARGS:
+        template_file(str) - name of Jinja2 template filename
+        params - parameters for templating. Usually dict.
+        template_dir - path to template dir ('tempaltes' by default)
+    RETURNS:
+        Just a rendered template
+    """
+    env = Environment(loader=FileSystemLoader(template_dir), trim_blocks=True, lstrip_blocks=True)
+    template = env.get_template(template_file)
+    return template.render(params)
+
+def config_device(connection, config_data, template):
+    configuration = cfg_from_template(template, config_data)
+    with manager.connect(**connection) as m:
+        result = m.edit_config(target='running', config=configuration)
+
 if __name__ == '__main__':
-    print_intfs_xmltodict(CONNECTION)
+    config_device(CONNECTION, CONFIG_DATA, 'loopback-template.j2')
+    result = print_intfs_xmltodict(CONNECTION)
